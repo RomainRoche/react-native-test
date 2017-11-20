810179106ad871ce151a3275c53de3d51283357f
The Wild Wine West Company <dev@wildwinewest.com>
2017-11-20 09:12:32 +0100

    cosmetics

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 import HomeScreen from './src/home-screen/Home';
-import WinesList from './src/wines-list/WinesList.js';
-import Wine from './src/wines-list/Wine.js';
 
 export default class App extends React.Component {
   
@@ -11,5 +9,5 @@ export default class App extends React.Component {
       <HomeScreen />
     );
   }
-  
+
 }
